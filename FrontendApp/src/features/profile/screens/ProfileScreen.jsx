84d77f90ac18e81bdaaca767414ebfafdbcdb6b6
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-06-21 00:23:49 -0600

    feat(frontend): crear ProfileScreen para navegación de tabs

---
+++ FrontendApp/src/features/profile/screens/ProfileScreen.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ClientHistoryScreen from '../../client/screens/ClientHistoryScreen';
+
+const ProfileScreen = () => {
+  return <ClientHistoryScreen />;
+};
+
+export default ProfileScreen;
