c63a184356fff26f89692a57ac1923a610c63f6f
iperez-2024003 <iperez-2024003@kinal.edu.gt>
2026-06-21 01:21:21 -0600

    feat agregar nuevos servicios de reservaciones eventos y revisiones al cliente axios

--- FrontendApp/src/shared/api/axiosClient.js
+++ FrontendApp/src/shared/api/axiosClient.js
@@ -61,24 +61,40 @@ export const authService = {
   register: (data) => axiosClient.post(`${API_CONFIG.AUTH_URL}/register`, data),
   getProfile: () => axiosClient.get(`${API_CONFIG.AUTH_URL}/profile`),
   updateProfile: (data) => axiosClient.put(`${API_CONFIG.AUTH_URL}/profile`, data),
+  changePassword: (data) => axiosClient.put(`${API_CONFIG.AUTH_URL}/profile/change-password`, data),
 };
 
 export const restaurantService = {
   getAll: () => axiosClient.get(`${API_CONFIG.RESTAURANT_URL}`),
   getById: (id) => axiosClient.get(`${API_CONFIG.RESTAURANT_URL}/${id}`),
   search: (query) => axiosClient.get(`${API_CONFIG.RESTAURANT_URL}/search`, { params: { q: query } }),
+  getTables: (restaurantId) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/tables`, { params: { restaurant_id: restaurantId } }),
+  createReview: (data) => axiosClient.post(`${API_CONFIG.RESTAURANT_SERVICE_URL}/reviews`, data),
+  getRestaurantReviews: (restaurantId) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/reviews/restaurant/${restaurantId}`),
 };
 
 export const menuService = {
-  getAll: () => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus`),
+  getAll: (restaurantId) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus`, { params: restaurantId ? { restaurant_id: restaurantId } : {} }),
   getById: (id) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus/${id}`),
-  getAllItems: () => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus/items/all`),
+  getAllItems: (params = {}) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus/items/all`, { params }),
   getItemById: (id) => axiosClient.get(`${API_CONFIG.RESTAURANT_SERVICE_URL}/menus/items/${id}`),
 };
 
 export const orderService = {
   create: (data) => axiosClient.post(`${API_CONFIG.ORDER_URL}`, data),
   getById: (id) => axiosClient.get(`${API_CONFIG.ORDER_URL}/${id}`),
+  getByUser: (userId) => axiosClient.get(`${API_CONFIG.ORDER_URL}`, { params: { user_id: userId } }),
+};
+
+export const reservationService = {
+  create: (data) => axiosClient.post(`${API_CONFIG.ORDER_SERVICE_URL}/reservations`, data),
+  checkAvailability: (params) => axiosClient.get(`${API_CONFIG.ORDER_SERVICE_URL}/reservations/check-availability`, { params }),
+  getByUser: (userId) => axiosClient.get(`${API_CONFIG.ORDER_SERVICE_URL}/reservations`, { params: { user_id: userId } }),
+};
+
+export const eventService = {
+  getAll: (params = {}) => axiosClient.get(`${API_CONFIG.REPORT_URL.replace('/stats', '')}/events`, { params }),
+  participate: (eventId, userId) => axiosClient.post(`${API_CONFIG.REPORT_URL.replace('/stats', '')}/events/${eventId}/participants`, { userId }),
 };
 
 export const reportService = {
