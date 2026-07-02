a60117ad9d2249173d985725b2a74554ab5d729f
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-07-01 19:17:21 -0600

    feat(nav): inicializar instancia de createNativeStackNavigator en AuthStack

--- FrontendApp/src/navigation/AuthStack.jsx
+++ FrontendApp/src/navigation/AuthStack.jsx
@@ -2,3 +2,5 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../features/auth/screens/LoginScreen';
 import RegisterScreen from '../features/auth/screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
