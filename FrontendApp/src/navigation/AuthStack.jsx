6a3f5c31d8e73475ca46916dcc845da1719c7626
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-07-01 19:18:54 -0600

    feat(nav): cerrar componente AuthStack y exportar módulo por defecto

--- FrontendApp/src/navigation/AuthStack.jsx
+++ FrontendApp/src/navigation/AuthStack.jsx
@@ -12,5 +12,10 @@ const AuthStack = () => {
         headerShown: false,
       }}
     >
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
