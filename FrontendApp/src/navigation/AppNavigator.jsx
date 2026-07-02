eaef369d88f7d6dd5f5442591821bf44fb671be7
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-07-01 19:12:43 -0600

    chore(nav): agregar todas las importaciones necesarias para AppNavigator

--- FrontendApp/src/navigation/AppNavigator.jsx
+++ FrontendApp/src/navigation/AppNavigator.jsx
@@ -1,1 +1,8 @@
-s
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthStack from './AuthStack';
+import MainTabNavigator from './MainTabNavigator';
+import RestaurantDetailScreen from '../features/restaurants/screens/RestaurantDetailScreen';
+import RestaurantMenuScreen from '../features/restaurants/screens/RestaurantMenuScreen';
+import ClientHistoryScreen from '../features/client/screens/ClientHistoryScreen';
