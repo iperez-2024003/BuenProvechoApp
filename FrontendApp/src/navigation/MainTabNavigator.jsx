fffd652012d53bd4b23b83c47c4ebfdda0b17b39
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-07-01 19:20:08 -0600

    chore(nav): agregar importaciones y pantallas para el navegador por pestañas

--- FrontendApp/src/navigation/MainTabNavigator.jsx
+++ FrontendApp/src/navigation/MainTabNavigator.jsx
@@ -1,1 +1,8 @@
-s
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from 'react-native';
+import { COLORS, SPACING, FONT_SIZE } from '../shared/constants/theme';
+import ClientDashboardScreen from '../features/client/screens/ClientDashboardScreen';
+import EventsScreen from '../features/events/screens/EventsScreen';
+import OrdersScreen from '../features/orders/screens/OrdersScreen';
+import ProfileScreen from '../features/profile/screens/ProfileScreen';
