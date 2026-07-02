bf5ddb3bdeacf8db5277101ee475a7e264b4dfcd
jmendez-2021550 <jmendez-2021550@kinal.edu.gt>
2026-07-01 19:17:08 -0600

    chore(nav): agregar importaciones de React, Navigation y pantallas de autenticación

--- FrontendApp/src/navigation/AuthStack.jsx
+++ FrontendApp/src/navigation/AuthStack.jsx
@@ -1,1 +1,4 @@
-s
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../features/auth/screens/LoginScreen';
+import RegisterScreen from '../features/auth/screens/RegisterScreen';
