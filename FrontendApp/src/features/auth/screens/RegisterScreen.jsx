0026b4226d584315d5a71ed2f048b94114f8a2f1
jaquino-2021159 <jaquino-2021159@kinal.edu.gt>
2026-07-01 19:46:26 -0600

    chore(auth): agregar importaciones base y componentes compartidos en RegisterScreen

--- FrontendApp/src/features/auth/screens/RegisterScreen.jsx
+++ FrontendApp/src/features/auth/screens/RegisterScreen.jsx
@@ -1,1 +1,15 @@
-s
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { COLORS, SPACING, FONT_SIZE } from '../../../shared/constants/theme';
+import Input from '../../../shared/components/common/Input';
+import Button from '../../../shared/components/common/Button';
+import { authService } from '../../../shared/api/axiosClient';
